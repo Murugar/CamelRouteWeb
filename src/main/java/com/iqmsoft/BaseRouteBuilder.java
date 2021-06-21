24dbd66deea2905949feae5094fb0adbde219f92
Muruga <davanon2014@gmail.com>
2021-06-21 12:31:51 +0530

    First

---
+++ src/main/java/com/iqmsoft/BaseRouteBuilder.java
@@ -0,0 +1,46 @@
+package com.iqmsoft;
+
+import com.datasonnet.document.DefaultDocument;
+import com.datasonnet.document.Document;
+import com.datasonnet.document.MediaTypes;
+import org.apache.camel.language.datasonnet.DatasonnetExpression;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
+
+/**
+ * This class centralizes exception handling for all Camel routes.  Extend this class for all RouteBuilder classes
+ * in the application.  Currently, two handlers are included:
+ * <ul>
+ * <li>RestException - malformed requests, etc.</li>
+ * <li>Exception - everything else returns a 500 status code.</li>
+ * </ul>
+ * These can also be used an examples of how to handle more specific exceptions.
+ *
+ * @author Maven Archetype (camel-oas-archetype)
+ */
+public class BaseRouteBuilder extends EndpointRouteBuilder {
+    protected DatasonnetExpression datasonnetEx(String expression) {
+        return (DatasonnetExpression) getContext().resolveLanguage("datasonnet").createExpression(expression);
+    }
+
+    protected DatasonnetExpression datasonnetEx(String expression, Class<?> resultType) {
+        Object[] properties = new Object[3];
+        properties[0] = resultType;
+        return (DatasonnetExpression) getContext().resolveLanguage("datasonnet").createExpression(expression, properties);
+    }
+
+    @Override
+    public void configure() throws Exception {
+
+        onException(Exception.class)
+            .routeId("exception-policy")
+            .handled(true)
+            .logHandled(true)
+            .logStackTrace(true)
+            .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(500))
+            .setBody(constant(DefaultDocument.NULL_INSTANCE))
+            .transform(datasonnetEx("resource:classpath:exception.ds", String.class)
+                    .outputMediaType(MediaTypes.APPLICATION_JSON))
+        ;
+    }
+}
---
+++ src/main/java/com/iqmsoft/RoutesImplementation.java
@@ -0,0 +1,24 @@
+package com.iqmsoft;
+
+import org.apache.camel.LoggingLevel;
+import org.springframework.stereotype.Component;
+
+/**
+ * The RoutesImplementation class holds implementations for the end points configured in RoutesScheduled.
+ *
+ * @author Maven Archetype (camel-scheduled-archetype)
+ */
+@Component
+public class RoutesImplementation extends BaseRouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        super.configure();
+
+        // Example of an implementation for a scheduled task end point.
+        from(direct("task"))
+            .log(LoggingLevel.INFO, "Task is running.")
+        ;
+
+    }
+}
